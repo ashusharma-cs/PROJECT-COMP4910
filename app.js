66ad3b7f7b12a29b85845a66d7e09701281d7e63
ashusharma-cs <ashu.lucifer@gmail.com>
2023-02-26 07:51:53 -0800

    added comments

--- app.js
+++ app.js
@@ -14,21 +14,28 @@ mongoose.set('strictQuery', false);
 // mongoose.connect(process.env.MONGO_URL);
 mongoose.connect("mongodb+srv://user_ashu:ashu@cluster1.hfx5czb.mongodb.net/tweetdatabase",{useNewUrlParser: true});
 
+
+//HOME ROUTE
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 
 })
 
+
+// CONNECTION BETWEEN PYTHON SCRIPT AND NODEJS
 let api="";
 PythonShell.run('mainjson.py', null).then(messages=>{
     api= require("./alltweets_filtered_keyword_relevant.json")
   });
 
 
+// JSON DATA ROUTE
 app.get("/service",(req,res)=>{
     res.send(api);
 })
 
+
+//DATABASE SCHEMA
 const tweetSchema = {
     id:Number,
     date: String,
@@ -41,9 +48,11 @@ const tweetSchema = {
     location: String
 }
 
+//CREATING A MODEL FOR OUR DATABASE
 const Tweet= mongoose.model("tweets", tweetSchema);
 
 
+// A QUERY TO FIND ALL TWEETS
 Tweet.find((err,tweets)=>{
     if(err)
     {
@@ -63,6 +72,8 @@ Tweet.find((err,tweets)=>{
 // newTweet.save()
 
 
+
+//LISTENING ON PORT 3000
 app.listen(3000, function(){
     console.log("Server Started.");
 })
