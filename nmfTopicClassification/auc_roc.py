4ab17c9581894c3de3b7842fc50fe5c317678e27
phoenixparisian <phoenixparisian@gmail.com>
2023-03-20 22:48:28 -0700

    Changed auc script to include SVD

--- nmfTopicClassification/auc_roc.py
+++ nmfTopicClassification/auc_roc.py
@@ -5,30 +5,38 @@ import matplotlib.pyplot as plt
 # Load data from CSV file into a Pandas DataFrame
 tweets_df = pd.read_csv('alltweets_filtered_labelled.csv')
 
-# Drop any rows with NaN values in either column
-tweets_df.dropna(subset=['relVirtualCare', 'nmf'], inplace=True)
+# Define columns to compute ROC curve for
+columns = ['nmf', 'svd']
 
-# Convert 0.5 in "relVirtualCare" column to 1
-tweets_df['relVirtualCare'] = tweets_df['relVirtualCare'].apply(lambda x: 1 if x == 0.5 else x)
+# Loop over columns and compute AUC score and ROC curve
+for col in columns:
+    # Drop any rows with NaN values in the current column
+    tweets_df.dropna(subset=['relVirtualCare', col], inplace=True)
 
-# Compute AUC score and ROC curve
-auc_score = roc_auc_score(tweets_df['relVirtualCare'], tweets_df['nmf'])
-fpr, tpr, thresholds = roc_curve(tweets_df['relVirtualCare'], tweets_df['nmf'])
+    # Convert 0.5 in "relVirtualCare" column to 1
+    tweets_df['relVirtualCare'] = tweets_df['relVirtualCare'].apply(lambda x: 1 if x == 0.5 else x)
 
-# Compute baseline ROC curve
-baseline_fpr = [0, 1]
-baseline_tpr = [0, 1]
+    # Compute AUC score and ROC curve
+    auc_score = roc_auc_score(tweets_df['relVirtualCare'], tweets_df[col])
+    fpr, tpr, thresholds = roc_curve(tweets_df['relVirtualCare'], tweets_df[col])
 
-# Plot ROC curve and baseline
-plt.plot(fpr, tpr, label=f"AUC={auc_score:.2f}")
-plt.plot(baseline_fpr, baseline_tpr, linestyle='--', color='gray', label='Baseline')
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('ROC Curve')
-plt.legend(loc="lower right")
+    # Compute baseline ROC curve
+    baseline_fpr = [0, 1]
+    baseline_tpr = [0, 1]
 
-# Save ROC curve as PNG file
-plt.savefig('roc_curve.png')
+    # Plot ROC curve and baseline
+    plt.plot(fpr, tpr, label=f"{col.upper()} AUC={auc_score:.2f}")
+    plt.plot(baseline_fpr, baseline_tpr, linestyle='--', color='gray', label='Baseline')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'ROC Curve ({col.upper()})')
+    plt.legend(loc="lower right")
 
-# Show ROC curve
-plt.show()
+    # Save ROC curve as PNG file
+    plt.savefig(f'roc_curve_{col}.png')
+
+    # Show ROC curve
+    plt.show()
+
+    # Clear plot for next iteration
+    plt.clf()
